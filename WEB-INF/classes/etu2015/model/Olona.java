7da5656fb13052b9390f935bc9b9b99830de5c52
Tsix <tsiresyrakotonirina02@gmail.com>
2023-03-19 23:24:15 +0300

    sprint3

---
+++ WEB-INF/classes/etu2015/model/Olona.java
@@ -0,0 +1,35 @@
+package etu2015.model;
+
+public class Olona {
+
+    int id;
+    String anarana;
+    
+    public void setId(int id) throws IllegalArgumentException {
+        if (id < 0) throw new IllegalArgumentException("id must be positive");
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setanarana(String anarana) throws IllegalArgumentException {
+        if (anarana == null) throw new IllegalArgumentException("argument vide");
+        this.anarana = anarana;
+    }
+
+    public String getanarana() {
+        return anarana;
+    }
+    
+    public Olona() {
+    
+    }
+    
+    public Olona(int id, String anarana) {
+        setId(id);
+        setanarana(anarana);
+    }
+
+}
